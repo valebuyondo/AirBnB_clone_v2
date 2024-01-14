1dd2221eb87914cd8d6e9eb824ee1c65cacd7dfb
valebuyondo <buyondovale@gmail.com>
2024-01-14 02:20:01 -0800

    3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+env.hosts = ["18.209.224.173", "54.242.188.222"]
 
 
 def do_pack():
